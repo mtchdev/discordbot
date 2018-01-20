1fe65cf267953ef6b08a8f0992a29b49a39b51b0
whenofficial <fsmitchm@gmail.com>
2018-01-20 14:03:26 +0400

    changed caps

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
-const discord = require('discord.js');
+const Discord = require('discord.js');
 const path = require('path');
-const client = new discord.Client();
+const Client = new discord.Client();
 const settings = require('./settings.json');
 
 client.on("ready", () => {
@@ -11,12 +11,12 @@ client.on('message', message => {
     var args = message.content.split(/[ ]+/);
 
     //shit goes here
-    if(message.content === settings.prefix + 'info'){
+    if (message.content === settings.prefix + 'info') {
         message.channel.send('Hey! I\'m just a lonely bot.');
         console.log('sent message to ' + message.author);
     }
-    if(message.content === settings.prefix + 'rank'){
-        if(message.member.roles.find("name", "cunt")){
+    if (message.content === settings.prefix + 'rank') {
+        if (message.member.roles.find("name", "cunt")) {
             message.reply('you are a cunt');
         } else {
             message.reply('you are a good boy');
