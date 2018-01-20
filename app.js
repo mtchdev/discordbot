6376cda64b641c279ba23216273fae48eb1a610f
whenofficial <fsmitchm@gmail.com>
2018-01-20 13:41:06 +0400

    fixed json and init app.js

--- app.js
+++ app.js
@@ -1,10 +1,18 @@
-const discord = require('discord');
+const discord = require('discord.js');
 const path = require('path');
 const client = new discord.Client();
-const settings = require('settings.json');
+const settings = require('./settings.json');
 
 client.on("ready", () => {
     console.log(settings.botName + ' started...');
+});
+
+client.on('message', message => {
+    var args = message.content.split(/[ ]+/);
+
+    //shit goes here
+
+
 });
 
 client.login(settings.token);
